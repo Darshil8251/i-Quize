f3951aa6629a00009ceab770e685a29a656a2f5c
Darshil8251 <106241346+Darshil8251@users.noreply.github.com>
2022-12-12 14:49:28 +0530

    final Upload

---
+++ PHP-Files/connect.php
@@ -0,0 +1,24 @@
+<?php
+
+    // $host='sql5.freesqldatabase.com';
+    // $user='sql5438305';
+    // $project='sql5438305';
+    // $ps='Fn66l1sWfz';
+
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $dbname = "singup2";
+
+
+
+    
+
+    $host='sql6.freesqldatabase.com';
+    $user='sql6581415';
+    $project='sql6581415';
+    $ps='YyeEh6YeaC';
+
+?>
+
+
